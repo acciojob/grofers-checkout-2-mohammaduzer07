4cf67ca4a288aedf2d43cb1d5016f9de053c83de
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2025-02-21 11:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+const table = document.querySelector('table')
+const lastRow = document.createElement('tr')
+const prices = document.querySelectorAll('[data-ns-test=prices]')
+
+let sum = 0
+for(let i = 0; i < prices.length; i++){
+	sum += parseInt(prices[i].innerText)
+}
+
+const newCol = document.createElement('td')
+newCol.innerText = sum
+newCol.setAttribute('data-ns-test', 'grandTotal')
+
+lastRow.appendChild(newCol)
+table.appendChild(lastRow)
